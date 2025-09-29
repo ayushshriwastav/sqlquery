5b22f8c452211a986104ef2c8509410ea4a455f7
ayushshriwstav <shriwastavayush22@gmail.com>
2025-09-29 18:20:21 +0530

    More Question Answer Added

--- if_else_plsql.sql
+++ if_else_plsql.sql
@@ -66,3 +66,57 @@ CALL check_number(15);
 CALL check_number(-7);   
 CALL check_number(0);   
 
+////
+
+delimiter //
+create procedure check_score(IN score int)
+begin
+    if score >= 90 then
+        select 'Excellent' AS result;
+    elseif score >= 75 then
+        select'Good' AS result;
+    elseif score >= 50 then
+        select'Average' AS result;
+    ELSE
+        select 'Fail' AS result;
+    end if;
+end //
+delimiter ;
+
+call check_score(86);
+
+
+////
+
+DELIMITER //
+
+create procedure check_leapYear(IN yr INT)
+begin
+    if ( yr % 400 = 0 or (yr % 100 != 0 and yr %4 = 0)) then
+    select concat(yr, ' is a leap year') as result;
+    else
+    select concat(yr, ' is NOT a Leap Year') as result;
+    end if;
+END //
+sys_config
+DELIMITER ;
+
+call check_leapYear(1804);
+
+////
+
+DELIMITER //
+
+create procedure check_vowelCharacter(IN ch varchar(1))
+begin
+    if (ch = 'a' or ch = 'e' or ch = 'i' or ch = 'o' or ch = 'u' or 
+    ch = 'A' or ch = 'E' or ch= 'I' or ch = 'O' or ch = 'U' ) then
+    select concat(ch, ' is a vowel') as result;
+    else
+    select concat(ch, ' is consonant') as result;
+    end if;
+END //
+sys_config
+DELIMITER ;
+
+call check_vowelCharacter('c');
